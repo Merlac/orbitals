f3b6a45a63220ecc9d305dac075b4b66d289c886
Tommi Honkala <tommi.honkala@datakala.fi>
2017-09-30 21:53:41 +0300

    Atmosphere test #1

--- js/planet.js
+++ js/planet.js
@@ -1,11 +1,14 @@
 // PLANET
 
+
+
+// CREATE PLANET
 function createPlanet(){
-  var planetGeometry = new THREE.SphereGeometry(200000,32,32);
+  var planetGeometry = new THREE.SphereGeometry(200000,64,64);
   var planetColor = parseInt ( getRandomColor().replace("#","0x"), 16 );
   console.log('Planet color: ' + planetColor);
   var planetMaterials = [
-  new THREE.MeshLambertMaterial( { opacity:0.9,
+  new THREE.MeshLambertMaterial( { opacity:1,
                                    color: planetColor,
                                    transparent:true } ),
   new THREE.MeshBasicMaterial( { color: 0xffffff,
@@ -15,4 +18,17 @@ function createPlanet(){
   planet = new THREE.SceneUtils.createMultiMaterialObject(planetGeometry, planetMaterials);
   planet.position.set(120000,-150000,-100000);
   scene.add(planet);
+
+  // create the mesh for the halo with AtmosphereMaterial
+  var geometry	= planetGeometry.clone()
+  THREEx.dilateGeometry(geometry, 0.5)
+  var material	= THREEx.createAtmosphereMaterial()
+  var meshHalo	= new THREE.Mesh(geometry, material );
+  meshHalo.position.set(120000,-150000,-100000);
+  meshHalo.scale.multiplyScalar(1.01);
+  scene.add( meshHalo );
+  // possible customisation of AtmosphereMaterial
+  material.uniforms.glowColor.value	= new THREE.Color('cyan')
+  material.uniforms.coeficient.value	= 1.1
+  material.uniforms.power.value		= 1.8
 }
